6f990392941b9cd72c89a8a771d1ad08b8866efa
DightMerc <dightmerc@gmail.com>
2020-07-12 15:00:47 +0500

    feat: client added | django orm connection in db.py

---
+++ src/bot/telegram/db.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import django
+
+from aiogram.types import InputFile
+
+import traceback
+
+proj_path = os.path.join(os.path.split(os.path.abspath(os.path.dirname(__file__)))[0])
+proj_path = os.path.join(os.path.split(proj_path)[0], 'engine')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "engine.settings")
+sys.path.append(proj_path)
+
+django.setup()
+
+import core.models as core_models
+import telegram.models as telegram_models
